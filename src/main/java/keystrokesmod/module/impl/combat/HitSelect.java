5d4441f974c1c73efc8d0d2d703c2220bf5e1dd7
Vaeyxx <leew2918@gmail.com>
2024-10-28 00:23:59 +0700

    uhm

--- src/main/java/keystrokesmod/module/impl/combat/HitSelect.java
+++ src/main/java/keystrokesmod/module/impl/combat/HitSelect.java
@@ -13,37 +13,30 @@ import org.jetbrains.annotations.NotNull;
 import static keystrokesmod.module.ModuleManager.hitSelect;
 
 public class HitSelect extends Module {
-    private static final String[] MODES = new String[]{"Pause", "Active"};
     private static final String[] PREFERENCES = new String[]{"Move Speed", "KB Reduction", "Critical Hit"};
     private final ModeSetting preference;
-    private final ModeSetting mode;
     private final SliderSetting delay;
-    private final SliderSetting chance;
 
     private static long attackTime = -1;
     private static boolean currentShouldAttack = false;
 
     public HitSelect() {
         super("HitSelect", category.combat, "Chooses the best time to hit.");
-        this.registerSetting(mode = new ModeSetting("Mode", MODES, 0,
-                "Pause: Legitimate pause clicking\n" +
-                "Active: Cancel attack but allow click"));
         this.registerSetting(preference = new ModeSetting("Preference", PREFERENCES, 0,
                 "Move Speed: Keep sprint but legitimate\n" +
                         "KB Reduction: KnockBack reduction\n" +
                         "Critical Hit: Critical hit frequency"));
         this.registerSetting(delay = new SliderSetting("Delay", 300, 0, 300, 1));
-        this.registerSetting(chance = new SliderSetting("Chance", 100, 0, 100, 1));
     }
 
     @Override
     public String getInfo() {
-        return MODES[(int) mode.getInput()];
+        return "Pause";
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onAttack(@NotNull AttackEntityEvent event) {
-        if (mode.getInput() == 1 && !currentShouldAttack) {
+        if (!currentShouldAttack) {
             event.setCanceled(true);
             return;
         }
@@ -54,26 +47,22 @@ public class HitSelect extends Module {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onPreUpdate(PreUpdateEvent event) {
-        currentShouldAttack = false;
+        currentShouldAttack = true;
 
-        if (Math.random() * 100 > hitSelect.chance.getInput()) {
-            currentShouldAttack = true;
-        } else {
-            switch ((int) preference.getInput()) {
-                case 0:
-                    currentShouldAttack = !mc.thePlayer.onGround && MoveUtil.isMoving();
-                    break;
-                case 1:
-                    currentShouldAttack = !mc.thePlayer.onGround && mc.thePlayer.motionY < 0;
-                    break;
-                case 2:
-                    currentShouldAttack = !mc.thePlayer.onGround && mc.thePlayer.fallDistance > 0.3;
-                    break;
-            }
-
-            if (!currentShouldAttack)
-                currentShouldAttack = System.currentTimeMillis() - HitSelect.attackTime >= hitSelect.delay.getInput();
+        switch ((int) preference.getInput()) {
+            case 0:
+                currentShouldAttack = mc.thePlayer.hurtTime > 0 && !mc.thePlayer.onGround && MoveUtil.isMoving();
+                break;
+            case 1:
+                currentShouldAttack = !mc.thePlayer.onGround && mc.thePlayer.motionY < 0;
+                break;
+            case 2:
+                currentShouldAttack = !mc.thePlayer.onGround && mc.thePlayer.fallDistance > 0.3;
+                break;
         }
+
+        if (!currentShouldAttack)
+            currentShouldAttack = System.currentTimeMillis() - HitSelect.attackTime >= hitSelect.delay.getInput();
     }
 
     public static boolean canAttack() {
@@ -81,7 +70,6 @@ public class HitSelect extends Module {
     }
 
     public static boolean canSwing() {
-        if (!hitSelect.isEnabled() || hitSelect.mode.getInput() == 1) return true;
-        return currentShouldAttack;
+        return hitSelect.isEnabled() && currentShouldAttack;
     }
 }
